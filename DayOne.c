d25a5ca5ffd6b5b2e505528f430cd27925ead7e3
meeloo1323 <sungjoon101@hotmail.com>
2024-12-19 00:51:17 -0800

    Finished Day 1 Solution
    Had to relearn some C nuances such as passing arrays into functions, and file I/O

--- DayOne.c
+++ DayOne.c
@@ -4,11 +4,76 @@ Within each pair, figure out how far apart the two numbers are; you'll need to a
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void merge(int *list, int leftIndex, int middleIndex, int rightIndex)
+{
+    int leftDummyIndex, rightDummyIndex, index;
+    int leftDummySize, rightDummySize;
+    leftDummySize = middleIndex - leftIndex + 1;
+    rightDummySize = rightIndex - middleIndex;
+    int *leftDummy, *rightDummy;
+    leftDummy = calloc(leftDummySize, sizeof(int));
+    rightDummy = calloc(rightDummySize, sizeof(int));
+
+    for (leftDummyIndex = 0; leftDummyIndex < leftDummySize; leftDummyIndex++)
+        leftDummy[leftDummyIndex] = list[leftIndex + leftDummyIndex];
+    for (rightDummyIndex = 0; rightDummyIndex < rightDummySize; rightDummyIndex++)
+        rightDummy[rightDummyIndex] = list[middleIndex + 1 + rightDummyIndex];
+    
+    leftDummyIndex = 0;
+    rightDummyIndex = 0;
+    index = leftIndex;
+
+    while (leftDummyIndex < leftDummySize && rightDummyIndex < rightDummySize)
+    {
+        if (leftDummy[leftDummyIndex] <= rightDummy[rightDummyIndex])
+        {
+            list[index] = leftDummy[leftDummyIndex];
+            leftDummyIndex++;
+        }
+        else
+        {
+            list[index] = rightDummy[rightDummyIndex];
+            rightDummyIndex++;
+        }
+        index++;
+    }
+    while (leftDummyIndex < leftDummySize)
+    {
+        list[index] = leftDummy[leftDummyIndex];
+        index++;
+        leftDummyIndex++;
+    }
+    while (rightDummyIndex < rightDummySize)
+    {
+        list[index] = rightDummy[rightDummyIndex];
+        index++;
+        rightDummyIndex++;
+    }
+
+    free(leftDummy);
+    free(rightDummy);
+}
+
+void mergeSort(int *list, int leftIndex, int rightIndex)
+{
+    if (leftIndex < rightIndex)
+    {
+
+        int middleIndex = (rightIndex - leftIndex) / 2 + leftIndex;
+
+        mergeSort(list, leftIndex, middleIndex);
+        mergeSort(list, middleIndex + 1, rightIndex);
+
+        merge(list, leftIndex, middleIndex, rightIndex);
+    }
+}
 
 int main()
 {
     FILE* file;
-    file = fopen("./DayOneInput.txt", "R");
+    file = fopen("DayOneInput.txt", "r");
 
     if (file == NULL)
     {
@@ -16,5 +81,24 @@ int main()
         exit(1);
     }
 
-    printf("Success");
+    int leftList[1000], rightList[1000]; // Knowing Advent of Code gives me 1000 lines of pairs, our assumption is that there is only 1000 pairs, and no more.
+    int index = 0;
+    char fileLine[50];
+    while (fgets(fileLine, 50, file) != NULL)
+    {
+        char *token = strtok(fileLine, " ");
+        leftList[index] = atoi(token);
+        token = strtok(NULL, " ");
+        rightList[index] = atoi(token);
+        index++;
+    }
+    fclose(file);
+
+    mergeSort(leftList, 0, 999);
+    mergeSort(rightList, 0, 999);
+
+    int sum = 0;
+    for (int i = 0; i < 1000; i++)
+        sum += abs(leftList[i] - rightList[i]);
+    printf("The sum of differences is: %i", sum);
 }
